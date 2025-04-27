b52cd1e7bee84e4e160c3c0458070a09bff5438c
Juan Felipe Garcia <jfelipegarcia23@gmail.com>
2025-04-27 01:26:14 -0400

    setting up database

--- server/db/migrations/20230423034706_create_users_table.js
+++ server/db/migrations/20230423034706_create_users_table.js
@@ -3,15 +3,18 @@
  * @returns { Promise<void> }
  */
 exports.up = (knex) => {
-  return knex.schema.createTable('users', (table) => {
-    table.increments();
-    table.string('username').notNullable().unique();
-    table.string('password_hash').notNullable();
-  })
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary(); // added id
+    table.string("username").notNullable().unique();
+    table.string("password_hash").notNullable();
+    table.string("zipcode").notNullable();
+    table.integer("exp").notNullable();
+    table.integer("level").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = (knex) => knex.schema.dropTable('users');
+exports.down = (knex) => knex.schema.dropTable("users");
--- server/db/seeds/init.js
+++ server/db/seeds/init.js
@@ -1,16 +1,33 @@
-const User = require('../../models/User');
+const User = require("../../models/User");
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.seed = async (knex) => {
-  await knex('users').del();
+  await knex("users").del();
+  await knex.raw("ALTER SEQUENCE users_id_seq RESTART WITH 1");
 
-  // resets user_id to 1 each time the seed file is executed.
-  await knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1');
-
-  // We could use `knex.raw` queries to create these users but we'll use the model
-  await User.create('cool_cat', '1234');
-  await User.create('l33t-guy', '1234');
-  await User.create('wowow', '1234');
+  await knex("users").insert([
+    {
+      username: "cool_cat",
+      password_hash: "1234",
+      zipcode: "12345",
+      exp: 0,
+      level: 1,
+    },
+    {
+      username: "l33t-guy",
+      password_hash: "1234",
+      zipcode: "67890",
+      exp: 10,
+      level: 2,
+    },
+    {
+      username: "wowow",
+      password_hash: "1234",
+      zipcode: "54321",
+      exp: 20,
+      level: 3,
+    },
+  ]);
 };
