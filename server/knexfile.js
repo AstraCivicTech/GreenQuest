eda441ef1d2ee6cfed932174b077507df40beaab
Ibrahim Hudson <you@example.com>
2025-05-22 16:33:09 -0400

    predeploy

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,44 +1,34 @@
 require("dotenv").config();
-console.log("Database:", process.env.PG_DB);
-console.log("User:", process.env.PG_USER);
-console.log("Password:", process.env.PG_PASS);
-const path = require("path");
-
-const migrationsDirectory = path.join(__dirname, "db/migrations");
-const seedsDirectory = path.join(__dirname, "db/seeds");
-
-/*
-We'll use environment variables to set the Postgres username and password
-so we don't share that information online.
-
-When we deploy in "production", we'll provide a PG_CONNECTION_STRING
-*/
 
 module.exports = {
   development: {
-    client: "pg",
-    connection: process.env.PG_CONNECTION_STRING || {
-      host: process.env.PG_HOST || "127.0.0.1",
-      port: process.env.PG_PORT || 5432,
-      user: process.env.PG_USER || "postgres",
-      password: process.env.PG_PASS || "123",
-      database: process.env.PG_DB || "GreenQuest",
+    client: "postgresql",
+    connection: {
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DB,
     },
     migrations: {
-      directory: migrationsDirectory,
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: seedsDirectory,
+      directory: "./db/seeds",
     },
   },
   production: {
-    client: "pg",
+    client: "postgresql",
     connection: process.env.PG_CONNECTION_STRING,
     migrations: {
-      directory: migrationsDirectory,
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: seedsDirectory,
+      directory: "./db/seeds",
+    },
+    pool: {
+      min: 2,
+      max: 10,
     },
   },
 };
