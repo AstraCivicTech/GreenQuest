e585b11d11a14c7711a0f1cc29b72cc489fecfb0
michaels2533 <michael.stawowski@wsu.edu>
2025-05-23 23:35:37 -0400

    Added SSL configurations to the producton environment

--- server/knexfile.js
+++ server/knexfile.js
@@ -35,7 +35,10 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.PG_CONNECTION_STRING,
+    connection: {
+      connectionString: process.env.PG_CONNECTION_STRING,
+      ssl:{rejectUnauthorized: false}
+    },
     migrations: {
       directory: migrationsDirectory,
     },
