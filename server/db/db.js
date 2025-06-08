a3dd5cbdd99f3cdc82c1b15ff77ba8fab1ce57b7
michaels2533 <michael.stawowski@wsu.edu>
2025-06-08 17:39:15 -0400

    Fixed the package.json file

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,4 @@
 const env = process.env.NODE_ENV || "development";
 const config = require("../knexfile")[env];
-module.exports = require("knex")(config);
+const knex  = require("knex")(config)
+module.exports = knex;
